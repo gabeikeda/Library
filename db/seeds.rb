cabed8367324c2f129e413e1055b1d70dbac1bf7
Gabe <gabeikeda@gmail.com>
2018-05-02 04:34:45 +0000

    finished project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Classifications = [
+  "General Works - encyclopedias",
+  "Philosophy, Psychology, Religion",
+  "History - Auxiliary Sciences",
+  "History (except American)",
+  "General U.S. History",
+  "Local U.S. History",
+  "Geography, Anthropology, Recreation",
+  "Social Sciences U",
+  "Political Science V",
+  "Law Z - Bibliography and Library Science",
+  "Education",
+  "Music",
+  "Fine Arts",
+  "Language and Literature",
+  "Science",
+  "Medicine",
+  "Agriculture",
+  "Technology",
+  "Military",
+  "Naval Science",
+  "Bibliography and Library Science"
+]
+
+Booktype = [
+"Fiction",
+"Non-Fiction"
+]
+
+Book.delete_all
+
+50.times do 
+  Book.create(
+            title: Faker::Book.title,
+            author: Faker::Book.author, 
+            genre: Faker::Book.genre,
+            classification: Classifications.sample,
+            book_type: Booktype.sample,
+            year: Faker::Number.between(1990, 2018)
+
+)
+end
